4e6a3b68a6d5e3c779d04437b89965f161beefa4
LI JIAHAO <lijiahao99131@gmail.com>
2018-10-07 21:29:26 +0800

    :tada: init project

---
+++ helpers.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"encoding/json"
+	"github.com/projectriri/bot-gateway/adapters/jsonrpc-server-any/jsonrpc-any"
+	"github.com/projectriri/bot-gateway/types"
+	"github.com/projectriri/bot-gateway/types/ubm-api"
+	"github.com/projectriri/bot-gateway/utils"
+	"math/rand"
+	"os"
+	"path/filepath"
+)
+
+const DATA_PATH = "data"
+
+func getFile(path string) (string, error) {
+	path = DATA_PATH + "/" + path
+	f, err := os.Stat(path)
+	if err != nil {
+		return "", err
+	}
+	if f.IsDir() {
+		files := make([]string, 0)
+		err := filepath.Walk(path, visit(&files))
+		if err != nil {
+			return "", err
+		}
+		f := files[rand.Intn(len(files))]
+		return f, nil
+	}
+	return path, nil
+}
+
+func visit(files *[]string) filepath.WalkFunc {
+	return func(path string, info os.FileInfo, err error) error {
+		if !info.IsDir() {
+			*files = append(*files, path)
+		}
+		return nil
+	}
+}
+
+func sendMessage(cid ubm_api.CID, msg ubm_api.Message) {
+	msg.CID = &cid
+	ubm := ubm_api.UBM{
+		Type:    "message",
+		Message: &msg,
+	}
+	b, _ := json.Marshal(ubm)
+	C.MakeRequest(jsonrpc_any.ChannelProduceRequest{
+		UUID: C.UUID,
+		Packet: types.Packet{
+			Head: types.Head{
+				UUID: utils.GenerateUUID(),
+				From: "haruka",
+				To:   cid.Messenger,
+				Format: types.Format{
+					API:     "ubm-api",
+					Version: "1.0",
+					Method:  "send",
+				},
+			},
+			Body: b,
+		},
+	})
+}
---
+++ main.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"encoding/json"
+	"github.com/BurntSushi/toml"
+	"github.com/projectriri/bot-gateway/adapters/jsonrpc-server-any/client/golang"
+	"github.com/projectriri/bot-gateway/router"
+	"github.com/projectriri/bot-gateway/types"
+	"github.com/projectriri/bot-gateway/types/cmd"
+	"github.com/projectriri/bot-gateway/types/ubm-api"
+	"io/ioutil"
+	"fmt"
+)
+
+var C jsonrpc_sdk.Client
+
+func main() {
+
+	_, err := toml.DecodeFile("config.toml", &config)
+	if err != nil {
+		panic(err)
+	}
+
+	C = jsonrpc_sdk.Client{}
+	C.Init(config.Addr, config.UUID)
+
+	C.Accept = []router.RoutingRule{
+		{
+			From: ".*",
+			To:   ".*",
+			Formats: []types.Format{
+				{
+					API:      "cmd",
+					Version:  "1.0",
+					Protocol: "",
+					Method:   "cmd",
+				},
+			},
+		},
+	}
+
+	C.Dial()
+	pkts, _ := C.GetUpdatesChan(0)
+	for pkt := range pkts {
+		var command cmd.Command
+		json.Unmarshal(pkt.Body, &command)
+		switch command.CmdStr {
+		case "haruka:echo":
+			if len(command.ArgsStr) == 0 {
+				continue
+			}
+			msg := ubm_api.Message{
+				Type: "rich_text",
+				RichText: &ubm_api.RichText{
+					{
+						Type: "text",
+						Text: command.ArgsStr,
+					},
+				},
+			}
+			sendMessage(command.Message.Chat.CID, msg)
+		case "haruka:sticker":
+			if len(command.ArgsTxt) == 0 {
+				continue
+			}
+			f, err := getFile("sticker/" + command.ArgsTxt[0])
+			if err != nil {
+				fmt.Println(err)
+				continue
+			}
+			b, err := ioutil.ReadFile(f)
+			if err != nil {
+				fmt.Println(err)
+				continue
+			}
+			msg := ubm_api.Message{
+				Type: "sticker",
+				Sticker: &ubm_api.Sticker{
+					Image: &ubm_api.Image{
+						Data: b,
+					},
+				},
+			}
+			sendMessage(command.Message.Chat.CID, msg)
+		}
+	}
+}
